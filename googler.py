0b0ed0bf30007cdabb0c470a6b8b48632348b553
Dilanka Kaushal Hewage <66146701+1nf1n17yk1ng@users.noreply.github.com>
2021-11-24 23:43:51 -0900

    Add files via upload

---
+++ googler.py
@@ -0,0 +1,19 @@
+try: # checks if googlesearch is installed in pip
+    from googlesearch import search
+except ImportError:
+    print("import error 'googlesearch'")
+    print("Run: pip3 install googlesearch") 
+
+def main():
+    # search string
+    q = input("What do you want to search up?: ")
+
+    # Save result
+    with open("google_search_history.txt", "w") as google_results:
+        for x in search(q, num=20, pause= 2):
+            print(x)
+            google_results.write(x + "\n")
+
+
+if __name__ == '__main__':
+    main()
